2e1f27253bdf8c36a3e61b97012b882366c57f5a
blaxxun <blaxxun@blaxxun.blaxxun>
2023-10-15 23:39:00 +0200

    v1.1.2

--- Guilds/Guilds.cs
+++ Guilds/Guilds.cs
@@ -16,7 +16,7 @@ namespace Guilds;
 public class Guilds : BaseUnityPlugin
 {
 	private const string ModName = "Guilds";
-	private const string ModVersion = "1.1.1";
+	private const string ModVersion = "1.1.2";
 	private const string ModGUID = "org.bepinex.plugins.guilds";
 
 	public static readonly ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
